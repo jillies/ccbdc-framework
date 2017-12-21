bc28004ea9e393ba10a4bab05b5ac40befb71b55
Jill Low <jilllow@maxis.com.my>
2017-12-21 17:02:16 +0800

    Changed name for colour variables based on guideline. Continued working on extensive button component.

--- includes/components/button.php
+++ includes/components/button.php
@@ -1,8 +1,100 @@
 <h6><strong>Button Component</strong></h6>
 
 <p><strong>Regular buttons</strong></p>
-<input type="submit" class="button--regular" value="Button" />
-<a href="#" class="button--regular">Button</a>
-<button type="submit" class="button--regular">Button</button>
+<input type="submit" class="button--regular" value="Regular Regular Regular Regular Regular Regular Regular Regular" /><br />
+<a href="#" class="button--regular">Regular Regular Regular Regular Regular Regular Regular Regular</a><br />
+<button type="submit" class="button--regular">Regular Regular Regular Regular Regular Regular Regular Regular</button>
+<br /><br />
 
+<p><strong>Small buttons</strong></p>
+<input type="submit" class="button--small" value="Small" /><br />
+<a href="#" class="button--small">Small</a><br />
+<button type="submit" class="button--small">Small</button>
+<br /><br />
+
+<p><strong>Large buttons</strong></p>
+<input type="submit" class="button--large" value="Large" /><br />
+<a href="#" class="button--large">Large</a><br />
+<button type="submit" class="button--large">Large</button>
+<br /><br />
+
+<p><strong>Types of buttons</strong></p>
+<input type="submit" class="button--regular outline" value="Outline" /><br />
+<a href="#" class="button--regular outline">Outline</a><br />
+<button type="submit" class="button--regular outline">Outline</button><br />
+
+<input type="submit" class="button--regular" value="Disabled" disabled /><br />
+<button type="submit" class="button--regular"  disabled>Disabled</button>
+<br /><br />
+
+<p><strong>Button colours</strong></p>
+<input type="submit" class="button--regular" value="Primary" /><br />
+<a href="#" class="button--regular">Primary</a><br />
+<button type="submit" class="button--regular">Primary</button><br />
+<input type="submit" class="button--regular secondary" value="Secondary" /><br />
+<a href="#" class="button--regular secondary">Secondary</a><br />
+<button type="submit" class="button--regular secondary">Secondary</button>
+<br /><br />
+
+<p><strong>Button with description</strong></p>
+<a href="#" class="button--description">Solid<br />
+	<span>Subtext here</span></a><br />
+<a href="#" class="button--description outline">Outline<br />
+	<span>Subtext here</span></a><br />
+<a href="#"  class="button--description disabled">Solid Disabled<br />
+	<span>Subtext here</span></a>
+<br /><br />
+
+<p><strong>Button with icon and text</strong></p>
+<a href="#" class="button--icontext">
+	<div class="button--icon">
+		<i class="material-icons">message</i>
+	</div>
+	<div class="button--text">
+		<h6>Button text</h6>
+		<p>Button subtext</p>
+	</div>
+	<div class="button--arrow">
+		<i class="material-icons">keyboard_arrow_right</i>
+	</div>
+</a>
+
+<a href="#" class="button--icontext left">
+	<div class="button--icon">
+		<i class="material-icons">message</i>
+	</div>
+	<div class="button--text">
+		<h6>Button text</h6>
+		<p>Button subtext</p>
+	</div>
+	<div class="button--arrow">
+		<i class="material-icons">keyboard_arrow_left</i>
+	</div>
+</a>
+
+<a href="#" class="button--icontext outline">
+	<div class="button--icon">
+		<i class="material-icons">layers</i>
+	</div>
+	<div class="button--text">
+		<h6>Button text</h6>
+		<p>Button subtext</p>
+	</div>
+	<div class="button--arrow">
+		<i class="material-icons">keyboard_arrow_right</i>
+	</div>
+</a>
+
+<a href="#" class="button--icontext left outline">
+	<div class="button--icon">
+		<i class="material-icons">layers</i>
+	</div>
+	<div class="button--text">
+		<h6>Button text</h6>
+		<p>Button subtext</p>
+	</div>
+	<div class="button--arrow">
+		<i class="material-icons">keyboard_arrow_left</i>
+	</div>
+</a>
 
