62b607d70aec149856ab3741ed724e5175be5b72
Jill Low <jilllow@maxis.com.my>
2018-01-05 16:48:50 +0800

    Added a border around floating top up button. Button component

--- includes/components/button.php
+++ includes/components/button.php
@@ -1,100 +1,271 @@
 <h6><strong>Button Component</strong></h6>
 
-<p><strong>Regular buttons</strong></p>
-<input type="submit" class="button--regular" value="Regular Regular Regular Regular Regular Regular Regular Regular" /><br />
-<a href="#" class="button--regular">Regular Regular Regular Regular Regular Regular Regular Regular</a><br />
-<button type="submit" class="button--regular">Regular Regular Regular Regular Regular Regular Regular Regular</button>
-<br /><br />
-
 <p><strong>Small buttons</strong></p>
-<input type="submit" class="button--small" value="Small" /><br />
-<a href="#" class="button--small">Small</a><br />
+<input type="submit" class="button--small" value="Small" /> 
+<a href="#" class="button--small">Small</a> 
 <button type="submit" class="button--small">Small</button>
 <br /><br />
 
+<p><strong>Regular buttons</strong></p>
+<input type="submit" class="button--regular" value="Regular" /> 
+<a href="#" class="button--regular">Regular</a> 
+<button type="submit" class="button--regular">Regular</button>
+<br /><br />
+
 <p><strong>Large buttons</strong></p>
-<input type="submit" class="button--large" value="Large" /><br />
-<a href="#" class="button--large">Large</a><br />
+<input type="submit" class="button--large" value="Large" /> 
+<a href="#" class="button--large">Large</a> 
 <button type="submit" class="button--large">Large</button>
 <br /><br />
 
 <p><strong>Types of buttons</strong></p>
-<input type="submit" class="button--regular outline" value="Outline" /><br />
-<a href="#" class="button--regular outline">Outline</a><br />
+<input type="submit" class="button--regular outline" value="Outline" /> 
+<a href="#" class="button--regular outline">Outline</a> 
 <button type="submit" class="button--regular outline">Outline</button><br />
 
-<input type="submit" class="button--regular" value="Disabled" disabled /><br />
-<button type="submit" class="button--regular"  disabled>Disabled</button>
+<input type="submit" class="button--regular" value="Disabled" disabled /> 
+<button type="submit" class="button--regular"  disabled>Disabled</button><br />
+<input type="submit" class="button--regular outline" value="Disabled Outline" disabled /> 
+<button type="submit" class="button--regular outline"  disabled>Disabled Outline</button>
 <br /><br />
 
 <p><strong>Button colours</strong></p>
-<input type="submit" class="button--regular" value="Primary" /><br />
-<a href="#" class="button--regular">Primary</a><br />
-<button type="submit" class="button--regular">Primary</button><br />
-<input type="submit" class="button--regular secondary" value="Secondary" /><br />
-<a href="#" class="button--regular secondary">Secondary</a><br />
-<button type="submit" class="button--regular secondary">Secondary</button>
-<br /><br />
+<input type="submit" class="button--regular primary" value="Primary" /> 
+<a href="#" class="button--regular primary">Primary</a> 
+<button type="submit" class="button--regular primary">Primary</button> 
+<input type="submit" class="button--regular primary outline" value="Primary Outline" /> 
+<a href="#" class="button--regular primary outline">Primary Outline</a> 
+<button type="submit" class="button--regular primary outline">Primary Outline</button><br />
+
+<input type="submit" class="button--regular secondary" value="Secondary" />
+<a href="#" class="button--regular secondary">Secondary</a>
+<button type="submit" class="button--regular secondary">Secondary</button> 
+<input type="submit" class="button--regular secondary outline" value="Secondary Outline" />
+<a href="#" class="button--regular secondary outline">Secondary Outline</a>
+<button type="submit" class="button--regular secondary outline">Secondary Outline</button><br />
+
+<input type="submit" class="button--regular warning" value="Warning" />
+<a href="#" class="button--regular warning">Warning</a>
+<button type="submit" class="button--regular warning">Warning</button> 
+<input type="submit" class="button--regular warning outline" value="Warning Outline" />
+<a href="#" class="button--regular warning outline">Warning Outline</a>
+<button type="submit" class="button--regular warning outline">Warning Outline</button><br />
+
+<input type="submit" class="button--regular alert" value="Alert" /> 
+<a href="#" class="button--regular alert">Alert</a> 
+<button type="submit" class="button--regular alert">Alert</button> 
+<input type="submit" class="button--regular alert outline" value="Alert Outline" /> 
+<a href="#" class="button--regular alert outline">Alert Outline</a> 
+<button type="submit" class="button--regular alert outline">Alert Outline</button>
+<br />
+<br />
+
+<p><strong>Maintaining button size in mobile</strong></p>
+<input type="submit" class="button--small inline-width" value="Small" /> 
+<a href="#" class="button--regular inline-width">Regular</a> 
+<button type="submit" class="button--large inline-width">Large</button>
+<input type="submit" class="button--small outline inline-width" value="Small" /> 
+<a href="#" class="button--regular secondary inline-width">Regular</a> 
+<button type="submit" class="button--large warning inline-width">Large</button>
+<br />
+<br />
 
 <p><strong>Button with description</strong></p>
 <a href="#" class="button--description">Solid<br />
-	<span>Subtext here</span></a><br />
+	<span>Subtext here</span></a> 
 <a href="#" class="button--description outline">Outline<br />
-	<span>Subtext here</span></a><br />
+	<span>Subtext here</span></a> 
 <a href="#"  class="button--description disabled">Solid Disabled<br />
-	<span>Subtext here</span></a>
+	<span>Subtext here</span></a> 
+<a href="#" class="button--description outline disabled">Outline Disabled<br />
+	<span>Subtext here</span></a><br />
+<a href="#" class="button--description primary">Solid<br />
+	<span>Subtext here</span></a> 
+<a href="#" class="button--description primary outline">Outline<br />
+	<span>Subtext here</span></a> 
+<a href="#" class="button--description secondary">Secondary<br />
+	<span>Subtext here</span></a> 
+<a href="#" class="button--description secondary outline">Secondary Outline<br />
+	<span>Subtext here</span></a> 
 <br /><br />
 
 <p><strong>Button with icon and text</strong></p>
 <a href="#" class="button--icontext">
-	<div class="button--icon">
-		<i class="material-icons">message</i>
-	</div>
-	<div class="button--text">
-		<h6>Button text</h6>
-		<p>Button subtext</p>
-	</div>
-	<div class="button--arrow">
-		<i class="material-icons">keyboard_arrow_right</i>
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">message</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text button subtext</h6>
+			<small class="font--smaller">Button subtext button subtext button subtext button subtext button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_right</i>
+		</div>
 	</div>
 </a>
 
 <a href="#" class="button--icontext left">
-	<div class="button--icon">
-		<i class="material-icons">message</i>
-	</div>
-	<div class="button--text">
-		<h6>Button text</h6>
-		<p>Button subtext</p>
-	</div>
-	<div class="button--arrow">
-		<i class="material-icons">keyboard_arrow_left</i>
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">message</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text button subtext</h6>
+			<small class="font--smaller">Button subtext button subtext button subtext button subtext button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_left</i>
+		</div>
 	</div>
 </a>
 
 <a href="#" class="button--icontext outline">
-	<div class="button--icon">
-		<i class="material-icons">layers</i>
-	</div>
-	<div class="button--text">
-		<h6>Button text</h6>
-		<p>Button subtext</p>
-	</div>
-	<div class="button--arrow">
-		<i class="material-icons">keyboard_arrow_right</i>
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">layers</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_right</i>
+		</div>
 	</div>
 </a>
 
 <a href="#" class="button--icontext left outline">
-	<div class="button--icon">
-		<i class="material-icons">layers</i>
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">layers</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_left</i>
+		</div>
 	</div>
-	<div class="button--text">
-		<h6>Button text</h6>
-		<p>Button subtext</p>
-	</div>
-	<div class="button--arrow">
-		<i class="material-icons">keyboard_arrow_left</i>
+</a>
+<br />
+<a href="#" class="button--icontext primary">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">message</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text button subtext</h6>
+			<small class="font--smaller">Button subtext button subtext button subtext button subtext button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_right</i>
+		</div>
 	</div>
 </a>
 
+<a href="#" class="button--icontext primary left">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">message</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_left</i>
+		</div>
+	</div>
+</a>
+
+<a href="#" class="button--icontext primary outline">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">layers</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_right</i>
+		</div>
+	</div>
+</a>
+
+<a href="#" class="button--icontext primary left outline">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">layers</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_left</i>
+		</div>
+	</div>
+</a>
+<br />
+<a href="#" class="button--icontext secondary">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">message</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text button subtext</h6>
+			<small class="font--smaller">Button subtext button subtext button subtext button subtext button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_right</i>
+		</div>
+	</div>
+</a>
+
+<a href="#" class="button--icontext secondary left">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">message</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_left</i>
+		</div>
+	</div>
+</a>
+
+<a href="#" class="button--icontext secondary outline">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">layers</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_right</i>
+		</div>
+	</div>
+</a>
+
+<a href="#" class="button--icontext secondary left outline">
+	<div class="button--icontext-wrap">
+		<div class="button--icontext-icon">
+			<i class="material-icons">layers</i>
+		</div>
+		<div class="button--icontext-text">
+			<h6>Button text</h6>
+			<small class="font--smaller">Button subtext</small>
+		</div>
+		<div class="button--icontext-arrow">
+			<i class="material-icons">keyboard_arrow_left</i>
+		</div>
+	</div>
+</a>
